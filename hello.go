0af2c6ccbf78e5e0ba3bfae24c9a3a9303c7c977
Mateus Silva <mateussilva@ntopus.com.br>
2023-09-09 12:14:03 -0300

    Objetos de configuração

--- hello.go
+++ hello.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!!!!!"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	w.Write([]byte(fmt.Sprintf("Hello, my name is %s, i'm %s", name, age)))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("App err: ", err)
+	}
+
+	w.Write([]byte(string(data)))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	w.Write([]byte(fmt.Sprintf("User: %s. Password: %s.", user, password)))
 }
