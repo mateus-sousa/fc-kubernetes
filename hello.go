7f58051e851145e4ac97f68379ab11236f0c7422
Mateus Silva <mateussilva@ntopus.com.br>
2023-09-10 12:21:28 -0300

    Probes

--- hello.go
+++ hello.go
@@ -6,12 +6,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/healthz", Healthz)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
@@ -24,9 +28,8 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	data, err := ioutil.ReadFile("/myfamily/family.txt")
 	if err != nil {
-		log.Fatalf("App err: ", err)
+		log.Fatal("App err: ", err)
 	}
-
 	w.Write([]byte(string(data)))
 }
 
@@ -34,4 +37,14 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	w.Write([]byte(fmt.Sprintf("User: %s. Password: %s.", user, password)))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("System Timeout: %f", duration.Seconds())))
+		return
+	}
+	w.Write([]byte("OK"))
 }
