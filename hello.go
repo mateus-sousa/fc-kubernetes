e653a14de2d19882fa81983ab814e06cd42da2bf
Mateus Silva <mateussilva@ntopus.com.br>
2023-09-12 13:05:58 -0300

    Resources e HPA

--- hello.go
+++ hello.go
@@ -41,7 +41,7 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(startedAt)
-	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+	if duration.Seconds() < 10 {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(fmt.Sprintf("System Timeout: %f", duration.Seconds())))
 		return
