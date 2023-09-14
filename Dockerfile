8d9f293fd8d79c35d4382df792b83cdb09541f61
Mateus Silva <mateussilva@ntopus.com.br>
2023-09-13 21:42:58 -0300

    StateFulSet e Volumes Persistentes

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY . .
 
+RUN apt-get update -y && apt-get install -y iputils-ping
+
 RUN go build -o hello .
 
 CMD ["./hello"]
---
+++ k8s/kube.sh
@@ -0,0 +1,8 @@
+
+kubectl apply -f k8s/configmap.yml
+kubectl apply -f k8s/configmap-family.yml 
+kubectl apply -f k8s/secret.yml 
+kubectl apply -f k8s/deployment.yml
+kubectl apply -f k8s/loadbalancer.yml
+kubectl apply -f k8s/metrics-server.yml
+kubectl apply -f k8s/hpa.yml 
